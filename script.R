49aa6b0eac7c37f451d9f45a03f0f08702cc7a7d
Dipanjan Deb <dipanjan.deb@ihs.com>
2015-04-09 00:03:36 +0530

    some training spilt code added

--- script.R
+++ script.R
@@ -1,4 +1,6 @@
 ottotrain<-read.csv(choose.files())
-sample(rows, size= 45350, replace=FALSE)->trainingindex
+sample(rows, size= 45350, replace=FALSE)->traininginde
 tr<-ottotrain[trainingindex,]
 ts<-ottotrain[-trainingindex,]
+xnam<-names(tr[,2:94])
+fmla <- as.formula(paste("target ~ ", paste(xnam, collapse= "+")))
