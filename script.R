fa93344eeb32529a14e8720b31bebe86d7fce674
Dipanjan Deb <dipanjan.deb@ihs.com>
2015-04-09 22:56:49 +0530

    added more caret functions

--- script.R
+++ script.R
@@ -6,3 +6,7 @@ xnam<-names(tr[,2:94])
 fmla <- as.formula(paste("target ~ ", paste(xnam, collapse= "+")))
 
 titanicDF <- read.csv('http://math.ucdenver.edu/RTutorial/titanic.txt',sep='\t')
+
+createDataPartition(ottotrain$target, p = .75, list=FALSE)->partition\
+ctrl<-trainControl(method="repeatedcv",repeats=5,classProbs=TRUE)
+gbmTune<-train(target~.,data=tr,method="gbm",metric="ROC",tuneGrid=grid,verbose=FALSE,trControl=ctrl)
