84d1171f420228f7b38fe764763d1f672def3c9e
Dipanjan Deb <dipanjan.deb@ihs.com>
2015-04-12 11:53:59 +0530

    added new code

---
+++ code2.R
@@ -0,0 +1,29 @@
+##Read the raw file
+ottotrain<-read_csv(choose.files())
+
+##draw 75% stratified sample for training and rest for testing
+createDataPartition(ottotrain$target, p = .75, list=FALSE)->partition
+tr<-ottotrain[partition,c(2:95)]
+row.names(tr)<-NULL
+
+ts<-ottotrain[-partition,c(2:95)]
+row.names(ts)<-NULL
+
+set.seed(1)
+## creating 5 fold cross validation for training control 
+fitControl <- trainControl(
+  method = "repeatedcv",
+  number = 5,
+  ## repeated ten times
+  repeats = 5)
+
+cl <- makeCluster(3)
+registerDoParallel(cl)
+## fitting a Gradient Boosting Machine with 5 CV training control
+set.seed(1)
+gbmFit <- train(target ~ ., data = tr,
+                 method = "gbm",
+                 trControl = fitControl,
+                 verbose = FALSE)
+
+stopCluster(cl)
